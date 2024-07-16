aaaf021aa6e432837b1cf7641f38aa1bb9708a19
Yanet Dagne <janepouvoir@gmail.com>
2024-07-16 19:46:56 +0300

    8th-mini project

---
+++ index.js
@@ -0,0 +1,10 @@
+let customerLoyalty= "bronze";
+if (customerLoyalty="bronze"){
+  console.log("10% off")
+}else if(customerLoyalty="silver"){
+  console.log("15% off")
+}else if(customerLoyalty="gold"){
+  console.log("20% off")
+}else{
+  console.log("No discount for unknown loyality level")
+}
